bffe17f660943cee6461322b7960642625205320
Antonio Ezequiel Liendro <65373420+Tony-L-93@users.noreply.github.com>
2021-05-30 11:12:19 -0300

    Feature/ce 265 listar profesores (#16)
    * All grade gets were created
    
    * Get grades were implented in Teacher Service Tets
    
    * Get grades were implented in Teacher Controller Tets

--- src/main/java/click/escuela/school/admin/repository/TeacherRepository.java
+++ src/main/java/click/escuela/school/admin/repository/TeacherRepository.java
@@ -1,5 +1,6 @@
 package click.escuela.school.admin.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import click.escuela.school.admin.model.Teacher;
 
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+
+	List<Teacher> findBySchoolId(Integer school);
+	
+	public List<Teacher> findByCourseId(UUID id);
+	
 
 }
