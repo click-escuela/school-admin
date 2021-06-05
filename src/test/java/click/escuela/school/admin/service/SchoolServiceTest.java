7184830743e4d9d44f060511d9bd52ad61859e67
Tony-L-93 <tonyliendro@gmail.com>
2021-06-04 23:28:25 -0300

    Service test was fixed

--- src/test/java/click/escuela/school/admin/service/SchoolServiceTest.java
+++ src/test/java/click/escuela/school/admin/service/SchoolServiceTest.java
@@ -1,6 +1,7 @@
 package click.escuela.school.admin.service;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 
 import java.util.UUID;
@@ -10,12 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import click.escuela.school.admin.api.SchoolApi;
+import click.escuela.school.admin.dto.SchoolDTO;
 import click.escuela.school.admin.enumerator.SchoolMessage;
 import click.escuela.school.admin.exception.TransactionException;
 import click.escuela.school.admin.mapper.Mapper;
@@ -30,14 +33,19 @@ public class SchoolServiceTest {
 	@Mock
 	private SchoolRepository schoolRepository;
 
+	@Mock
+	ModelMapper modelMapper;
+	
 	private SchoolServiceImpl schoolServiceImpl = new SchoolServiceImpl();
 	private SchoolApi schoolApi;
 	private School school;
+	private SchoolDTO schoolDTO;
 	private UUID id;
 
 	@Before
 	public void setUp() {
 		PowerMockito.mockStatic(Mapper.class);
+		modelMapper = new ModelMapper();
 		id = UUID.randomUUID();
 		school = School.builder().id(id).name("Colegio Nacional").cellPhone("47589869")
 				.email("colegionacional@edu.gob.com").adress("Entre Rios 1418").countCourses(10).countStudent(20)
@@ -45,7 +53,8 @@ public class SchoolServiceTest {
 		schoolApi = SchoolApi.builder().name("Colegio Nacional").cellPhone("47589869")
 				.email("colegionacional@edu.gob.com").adress("Entre Rios 1418").countCourses(10).countStudent(20)
 				.build();
-
+		schoolDTO = new SchoolDTO();
+		
 		Mockito.when(Mapper.mapperToSchool(schoolApi)).thenReturn(school);
 		Mockito.when(schoolRepository.save(school)).thenReturn(school);
 
@@ -56,7 +65,7 @@ public class SchoolServiceTest {
 	@Test
 	public void whenCreateIsOk() throws TransactionException {
 		schoolServiceImpl.create(schoolApi);
-		verify(schoolRepository).save(school);
+		verify(schoolRepository).save(Mapper.mapperToSchool(schoolApi));
 	}
 
 	@Test
@@ -66,7 +75,19 @@ public class SchoolServiceTest {
 		assertThatExceptionOfType(TransactionException.class).isThrownBy(() -> {
 			schoolServiceImpl.create(Mockito.any());
 		}).withMessage(SchoolMessage.CREATE_ERROR.getDescription());
+	}
+	
+	@Test
+	public void convertSchoolToSchoolDTO() {
+		schoolDTO = modelMapper.map(school, SchoolDTO.class);
+		assertEquals(school.getName(), schoolDTO.getName());
+	}
 
+	@Test
+	public void convertSchoolApiToSchool() {
+		school = new School();
+		school = modelMapper.map(schoolApi, School.class);
+		assertEquals(school.getName(), schoolApi.getName());
 	}
 
 }
