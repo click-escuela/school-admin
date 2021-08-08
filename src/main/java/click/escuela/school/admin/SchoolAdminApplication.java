939a6958595a7876ffff602dd491bcfb14d24cce
oscarumbert34 <oscar.umbert@gmail.com>
2021-08-08 11:35:22 -0300

    Spring cloud (#31)
    * configure eureka
    
    * application name was removed

--- src/main/java/click/escuela/school/admin/SchoolAdminApplication.java
+++ src/main/java/click/escuela/school/admin/SchoolAdminApplication.java
@@ -2,7 +2,9 @@ package click.escuela.school.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SchoolAdminApplication {
 
