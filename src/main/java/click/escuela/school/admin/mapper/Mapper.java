33f60ff470ea6ede17e0e9a6b0e7b4ed01009597
Antonio Ezequiel Liendro <65373420+Tony-L-93@users.noreply.github.com>
2021-08-25 08:26:49 -0300

    Feature/ce 382 listar nota por cursos del profesor (#38)
    * Get courses by teacher id was created
    
    * Student was added in course and grade belongs to studetn now
    
    * refactor was added in getCoursesByTeacherId
    
    * Teacher service test were added
    
    * Teacher controller tests were added
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>

--- src/main/java/click/escuela/school/admin/mapper/Mapper.java
+++ src/main/java/click/escuela/school/admin/mapper/Mapper.java
@@ -20,9 +20,7 @@ import click.escuela.school.admin.dto.BillDTO;
 import click.escuela.school.admin.dto.CourseDTO;
 import click.escuela.school.admin.dto.CourseStudentsDTO;
 import click.escuela.school.admin.dto.CourseStudentsShortDTO;
-
 import click.escuela.school.admin.dto.ExcelDTO;
-
 import click.escuela.school.admin.dto.SchoolDTO;
 import click.escuela.school.admin.dto.StudentDTO;
 import click.escuela.school.admin.dto.StudentShortDTO;
