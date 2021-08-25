c346f14b4f3b90abf3c03de01106995172a3a754
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-08-24 21:31:49 -0300

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,5 +5,5 @@ sudo rm -rf /home/ec2-user/server/school-admin/school-service.pid
 
 echo "eliminando archvo"
 
-sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
+sudo java -jar -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
     school-admin-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > school-service.pid
