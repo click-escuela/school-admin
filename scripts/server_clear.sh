c8c59e955fe821163d7574bd5d66d78fc898e935
Evelin <eve_aragon@hotmail.com>
2021-10-18 23:04:35 -0300

    subir a dev

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/school-admin
+sudo rm -rf /home/ec2-user/server/school-admin/appspec.yml
+sudo rm -rf /home/ec2-user/server/school-admin/school-admin-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/school-admin/school-service.pid
+sudo rm -rf /home/ec2-user/server/school-admin/logs
+sudo rm -rf /home/ec2-user/server/school-admin/server_stop.sh
+sudo rm -rf /home/ec2-user/server/school-admin/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/school-admin/server_prestart.sh
+sudo rm -rf /home/ec2-user/server/school-admin/server_clear.sh
+
---
+++ scripts/server_prestart.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/ec2-user/server/school-admin
+sudo sh server_start.sh
